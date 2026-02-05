2f23e615744282ef038ae0a5dbc0649be50c06a5
rafasilv <rafaelasoperes@gmail.com>
2026-02-05 18:16:25 -0300

    Estava muito zoado antes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rafasilv <rafasilv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 10:59:54 by rafinha           #+#    #+#             */
-/*   Updated: 2026/02/04 09:08:33 by rafasilv         ###   ########.fr       */
+/*   Updated: 2026/02/05 18:12:58 by rafasilv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,58 +15,9 @@
 
 #include <stdio.h>
 
-int	ft_atoi_push(const char *str)
+
+
+int main(void) 
 {
-	int	i;
-	int	num;
-	int	sign;
 
-	i = 0;
-	num = 0;
-	sign = 1;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		num = num * 10 + (str[i] - '0');
-		i++;
-	}
-    
-	return (num * sign);
-}
-
-
-
-int main(int argc, char **argv) 
-{
-    int i;
-    int j;
-    int str;
-
-    i = 1;
-    j = 0;
-    if (argc < 2) 
-    {
-        write(2, "Error\n", 6);
-        return (-1);
-    }
-
-    while (argv[i][j])
-    {
-        j = 0;
-        while (argv[i][j] != '\0')
-        {
-            str = ft_atoi_push(&argv[i][j]);
-            j++;
-        }
-        i++;
-    }
-    printf("Teste: %d", str);
-    return 0;
 }
